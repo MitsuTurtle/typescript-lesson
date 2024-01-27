4303aec967fc3064b907c52e5bb50acf9a65fcad
Mitsuhiro Kawashima <mshimashima@gmail.com>
2024-01-27 11:13:13 +0900

    boolean型とnumber型とstring型

---
+++ index.js
@@ -0,0 +1,7 @@
+var hasValue = false;
+var count = 34;
+var float = 3.5;
+var negative = -5.6;
+var single = 'hello';
+var double = 'world';
+var back = "hello";
---
+++ index.ts
@@ -0,0 +1,7 @@
+let hasValue: boolean = false;
+let count: number = 34;
+let float: number = 3.5;
+let negative: number = -5.6;
+let single: string = 'hello';
+let double: string = 'world';
+let back: string = `hello`;
