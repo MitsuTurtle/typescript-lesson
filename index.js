799ddb18d42996ea43296817886cd8e529c9e467
Mitsuhiro Kawashima <mshimashima@gmail.com>
2024-01-30 21:39:07 +0900

    tsconfig.jsonですべてのファイルを一気にコンパイル

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-var hasValue = false;
-var count = 34;
-var float = 3.5;
-var negative = -5.6;
-var single = 'hello';
-var double = 'world';
-var back = "hello";
-var hello;
+"use strict";
+let hasValue = false;
+let count = 34;
+let float = 3.5;
+let negative = -5.6;
+let single = 'hello';
+let double = 'world';
+let back = `hello`;
+let hello;
 hello = 'hello';
-var person = {
+const person = {
     name: {
         first: 'Jack',
         last: 'Smith',
     },
     age: 21,
 };
-var fruits = ['Apple', 'Banana', 'Grape'];
-var book = ['business', 1500, false];
+const fruits = ['Apple', 'Banana', 'Grape'];
+const book = ['business', 1500, false];
 var CoffeeSize;
 (function (CoffeeSize) {
     CoffeeSize[CoffeeSize["SHORT"] = 0] = "SHORT";
@@ -23,18 +24,18 @@ var CoffeeSize;
     CoffeeSize[CoffeeSize["GRANDE"] = 2] = "GRANDE";
     CoffeeSize[CoffeeSize["VENTI"] = 3] = "VENTI";
 })(CoffeeSize || (CoffeeSize = {}));
-var coffee = {
+const coffee = {
     hot: true,
     size: CoffeeSize.TALL,
 };
 console.log(CoffeeSize.SHORT);
-var unionType = 10;
+let unionType = 10;
 unionType = 'hello';
-var unionTypes = [21, 'hello'];
-var apple = 'apple';
-var banana = 2;
-var clothSize = 'small';
-var cloth = {
+let unionTypes = [21, 'hello'];
+const apple = 'apple';
+const banana = 2;
+let clothSize = 'small';
+const cloth = {
     color: 'white',
     size: 'medium',
 };
@@ -46,22 +47,22 @@ function sayHello() {
     console.log('Hello');
 }
 console.log(sayHello());
-var tmp;
-var anotherAdd = function (num1, num2) {
+let tmp;
+const anotherAdd = function (num1, num2) {
     return num1 + num2;
 };
-var doubleNumber = function (number) { return number * 2; };
-var doubleNumber2 = function (num) { return num * 2; };
+const doubleNumber = (number) => number * 2;
+const doubleNumber2 = (num) => num * 2;
 function doubleAndHandle(num, cb) {
-    var doubleNum = cb(num * 2);
+    const doubleNum = cb(num * 2);
     console.log(doubleNum);
 }
-doubleAndHandle(2, function (doubleNum) {
+doubleAndHandle(2, (doubleNum) => {
     return doubleNum;
 });
-var unknownInput;
-var anyInput;
-var text;
+let unknownInput;
+let anyInput;
+let text;
 unknownInput = 'hello';
 unknownInput = 21;
 unknownInput = true;
