62467f787d59597b4c9bf85948ab8d63786f171f
Mitsuhiro Kawashima <mshimashima@gmail.com>
2024-02-03 17:49:34 +0900

    クラスを型として使う

--- src/class.ts
+++ src/class.ts
@@ -3,6 +3,17 @@ class Person {
   constructor(initName: string) {
     this.name = initName;
   }
+
+  greeting(this: Person) {
+    console.log(`Hello! My name is ${this.name}`);
+  }
 }
+let person2: Person;
 const quill = new Person('Quill');
 console.log(quill);
+quill.greeting();
+const anotherQuill = {
+  name: 'AnotherQuill',
+  greeting: quill.greeting,
+};
+anotherQuill.greeting();
