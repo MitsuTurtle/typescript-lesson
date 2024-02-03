dbd9bcc4084a7e53c1afdbc7c5cf6da37724e356
Mitsuhiro Kawashima <mshimashima@gmail.com>
2024-02-03 18:11:48 +0900

    readonly就職子を使う

--- src/class.ts
+++ src/class.ts
@@ -1,5 +1,10 @@
 class Person {
-  constructor(public name: string, private age: number) {}
+  readonly id: number = 32;
+  constructor(readonly name: string, private age: number) {
+    this.id = 31;
+    this.id = 30;
+    this.name = 'hoge';
+  }
 
   incrementAge() {
     this.age += 1;
@@ -12,4 +17,6 @@ let person2: Person;
 const quill = new Person('Quill', 38);
 quill.incrementAge();
 console.log(quill);
+console.log(quill.id);
+console.log(quill.name);
 quill.greeting();
