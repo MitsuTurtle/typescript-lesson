864c3c06bb3f4fa9835b059fa23011d181682feb
Mitsuhiro Kawashima <mshimashima@gmail.com>
2024-02-03 22:30:35 +0900

    構造的部分型

--- src/interface.ts
+++ src/interface.ts
@@ -11,3 +11,13 @@ class Developer implements Human {
     console.log('Hello');
   }
 }
+const tempDeveloper = {
+  name: 'Quill',
+  age: 38,
+  experience: 3,
+  greeting(message: string) {
+    console.log(message);
+  },
+};
+// const user: Human = new Developer('Quill', 38, 3);
+const user: Human = tempDeveloper;
