6589d886833ca8db177b540b28971c76e235f891
Mitsuhiro Kawashima <mshimashima@gmail.com>
2024-03-05 23:00:09 +0900

    namespaceの使い方

--- src/lodash.d.ts
+++ src/lodash.d.ts
@@ -1,7 +1,9 @@
 declare module 'lodash' {
   export function shuffle<T>(arr: T[]): T[];
 }
-interface Lodash {
-  shuffle<T>(arr: T[]): T[];
+// interface Lodash {
+//   shuffle<T>(arr: T[]): T[];
+// }
+declare namespace _ {
+  function shuffle<T>(arr: T[]): T[];
 }
-declare const _: Lodash;
