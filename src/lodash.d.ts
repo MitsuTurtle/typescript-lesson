f3aebb33efae9bab04b27d876e0e33ff7f9f969d
Mitsuhiro Kawashima <mshimashima@gmail.com>
2024-03-07 22:22:21 +0900

    lodashの型定義ファイル

--- src/lodash.d.ts
+++ src/lodash.d.ts
@@ -1,9 +1,9 @@
-declare module 'lodash' {
-  export function shuffle<T>(arr: T[]): T[];
-}
-// interface Lodash {
-//   shuffle<T>(arr: T[]): T[];
+// declare module 'lodash' {
+//   export function shuffle<T>(arr: T[]): T[];
 // }
-declare namespace _ {
-  function shuffle<T>(arr: T[]): T[];
-}
+// // interface Lodash {
+// //   shuffle<T>(arr: T[]): T[];
+// // }
+// declare namespace _ {
+//   function shuffle<T>(arr: T[]): T[];
+// }
