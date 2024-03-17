f9132812deaf787a0b80290056dd017ff6683f16
Mitsuhiro Kawashima <mshimashima@gmail.com>
2024-03-17 16:02:58 +0900

    Expressを使ってルーティングの処理をする

--- typescript-node/dist/app.js
+++ typescript-node/dist/app.js
@@ -1,6 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// import * as express from 'express';
 const express = require("express");
-// import express = require('express');
 const app = express();
+app.use('/', (req, res, next) => {
+    console.log('Good!');
+    next();
+});
+app.get('/', (req, res, next) => {
+    res.send('<hq>Hello</hq>');
+});
+// app.use('/', (err, req, res, next) => {
+//   next();
+// });
 app.listen(3000);
--- typescript-node/src/app.ts
+++ typescript-node/src/app.ts
@@ -1,4 +1,14 @@
-import * as express from 'express';
-// import express = require('express');
+// import * as express from 'express';
+import express = require('express');
 const app = express();
+app.use('/', (req, res, next) => {
+  console.log('Good!');
+  next();
+});
+app.get('/', (req, res, next) => {
+  res.send('<hq>Hello</hq>');
+});
+// app.use('/', (err, req, res, next) => {
+//   next();
+// });
 app.listen(3000);
